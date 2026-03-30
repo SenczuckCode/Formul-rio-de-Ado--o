7a950ba49fe23372d41bed0aa638fc6d41be4d7b
SenczuckCode <lucas_senczuck@estudante.sesisenai.org.br>
2026-03-30 09:08:22 -0300

    Feat - começando a programar

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("formulario").addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let cpf = document.getElementById("cpf").value;
+    let idade = document.getElementById("idade").value;
+    let cidade = document.getElementById("cidade").value;
+    let quintal = document.getElementById("quintal").value; 
+    let pet = document.getElementById('input[name="pet"].checked;
+
